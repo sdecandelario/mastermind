b3c66a5419f93e6704834a500e6b010abf53f45f
sdecandelario <sergio.decandelario@gmail.com>
2023-03-24 17:11:57 +0100

    New endpoint to create a new game, test adapted to check that the status code is the expected (201)

--- tests/Functional/MastermindContext/Infrastructure/Game/Http/Controller/CreateGameControllerTest.php
+++ tests/Functional/MastermindContext/Infrastructure/Game/Http/Controller/CreateGameControllerTest.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Functional\MastermindContext\Infrastructure\Game\Http\Controller;
+namespace App\Tests\Functional\MastermindContext\Infrastructure\Game\Http\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 final class CreateGameControllerTest extends WebTestCase
 {
@@ -13,5 +14,7 @@ final class CreateGameControllerTest extends WebTestCase
         $client = self::createClient();
 
         $client->request('POST', '/api/game');
+
+        self::assertSame(Response::HTTP_CREATED, $client->getResponse()->getStatusCode());
     }
 }
