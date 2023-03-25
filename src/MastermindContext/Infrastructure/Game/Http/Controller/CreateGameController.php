a8bc736240cff80494ee954e0d52d3c64e5439d4
sdecandelario <sergio.decandelario@gmail.com>
2023-03-25 09:41:07 +0100

    Create new game test pass because we return a valid uuid

--- src/MastermindContext/Infrastructure/Game/Http/Controller/CreateGameController.php
+++ src/MastermindContext/Infrastructure/Game/Http/Controller/CreateGameController.php
@@ -6,11 +6,12 @@ namespace App\MastermindContext\Infrastructure\Game\Http\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Uid\Uuid;
 
 final class CreateGameController
 {
     public function __invoke(): Response
     {
-        return new JsonResponse(['id' => 'fakeId'], Response::HTTP_CREATED);
+        return new JsonResponse(['id' => Uuid::v4()->toRfc4122()], Response::HTTP_CREATED);
     }
 }
