a8a94437a20c9ff2b2dac522ea706c9647245ba7
sdecandelario <sergio.decandelario@gmail.com>
2023-03-26 20:44:33 +0200

    Test failing when we try to make a guess on a non existing game

---
+++ src/MastermindContext/Application/Guess/Command/MakeGuessCommandHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MastermindContext\Application\Guess\Command;
+
+use App\Shared\Domain\Command\CommandInterface;
+
+final class MakeGuessCommandHandler
+{
+    public function __invoke(CommandInterface $command)
+    {
+    }
+}
--- tests/Functional/MastermindContext/Infrastructure/Guess/Http/MakeGuessController.php
+++ tests/Functional/MastermindContext/Infrastructure/Guess/Http/MakeGuessController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\MastermindContext\Infrastructure\Guess\Http;
 
+use App\MastermindContext\Domain\Game\GameId;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -20,5 +21,19 @@ final class MakeGuessController extends WebTestCase
         $jsonResponse = json_decode($client->getResponse()->getContent(), true);
 
         self::assertSame(['error' => "The id 'anId' is not a valid uuid"], $jsonResponse);
+    }
+
+    public function testGameNotFound()
+    {
+        $client = self::createClient();
+        $gameId = GameId::create();
+
+        $client->request('POST', "/api/game/$gameId/guess");
+
+        self::assertSame(Response::HTTP_NOT_FOUND, $client->getResponse()->getStatusCode());
+
+        $jsonResponse = json_decode($client->getResponse()->getContent(), true);
+
+        self::assertSame(['error' => "Game with id {$gameId->id()->__toString()} not found"], $jsonResponse);
     }
 }
