9b4575926b45c931e3796ecd91575c4790fbbeda
sdecandelario <sergio.decandelario@gmail.com>
2023-03-28 18:45:33 +0200

    Added test to check that a finished game is showing the secret color code, test fail for lack of logic

--- tests/Functional/MastermindContext/Infrastructure/Game/Http/GetGameTest.php
+++ tests/Functional/MastermindContext/Infrastructure/Game/Http/GetGameTest.php
@@ -6,6 +6,7 @@ namespace App\Tests\Functional\MastermindContext\Infrastructure\Game\Http;
 
 use App\MastermindContext\Domain\Game\GameId;
 use App\MastermindContext\Domain\Game\GameRepositoryInterface;
+use App\MastermindContext\Domain\Game\GameStatus;
 use App\Tests\Builder\GameBuilder;
 use App\Tests\Builder\GuessBuilder;
 use Doctrine\ORM\EntityManagerInterface;
@@ -71,6 +72,62 @@ final class GetGameTest extends WebTestCase
         self::assertSame([
             'id' => $game->id()->__toString(),
             'status' => $game->status()->value,
+            'guesses' => [
+                [
+                    'id' => $guess->id()->__toString(),
+                    'colorCode' => $guess->colorCode()->value(),
+                    'blackPeg' => $guess->blackPeg(),
+                    'whitePeg' => $guess->whitePeg(),
+                ],
+            ],
+        ], $jsonResponse);
+    }
+
+    public function finishedGameStatusProvider(): array
+    {
+        return [
+            [GameStatus::Lost],
+            [GameStatus::Won],
+        ];
+    }
+
+    /**
+     * @dataProvider finishedGameStatusProvider
+     */
+    public function testGameFinishedRetrievedShowTheSecretCode(GameStatus $status): void
+    {
+        $client = self::createClient();
+        $entityManager = self::getContainer()->get(EntityManagerInterface::class);
+
+        $id = GameId::create();
+        $game = GameBuilder::create()
+            ->withId($id)
+            ->withStatus($status)
+            ->build();
+        $guess = GuessBuilder::create($game)->build();
+        $game->addGuess($guess);
+
+        $entityManager->persist($game);
+        $entityManager->flush();
+
+        $client->request('GET', "/api/game/$id");
+
+        $jsonResponse = json_decode($client->getResponse()->getContent(), true);
+
+        self::assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        self::assertArrayHasKey('id', $jsonResponse);
+        self::assertArrayHasKey('status', $jsonResponse);
+
+        /**
+         * @var GameRepositoryInterface $gameRepository
+         */
+        $gameRepository = self::getContainer()->get(GameRepositoryInterface::class);
+        $game = $gameRepository->findById($id);
+
+        self::assertSame([
+            'id' => $game->id()->__toString(),
+            'status' => $game->status()->value,
+            'secretCode' => $game->secretCode()->value(),
             'guesses' => [
                 [
                     'id' => $guess->id()->__toString(),
