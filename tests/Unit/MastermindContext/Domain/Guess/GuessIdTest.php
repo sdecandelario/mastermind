08eff60708dad639d0cd72028b23569e126be6c9
sdecandelario <sergio.decandelario@gmail.com>
2023-03-29 19:51:40 +0200

    Unit test for GuessId

---
+++ tests/Unit/MastermindContext/Domain/Guess/GuessIdTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Unit\MastermindContext\Domain\Guess;
+
+use App\MastermindContext\Domain\Guess\Exception\InvalidGuessIdException;
+use App\MastermindContext\Domain\Guess\GuessId;
+use PHPUnit\Framework\TestCase;
+
+class GuessIdTest extends TestCase
+{
+    public function testCreateFromStringWithInvalidFormadThrowException(): void
+    {
+        $this->expectException(InvalidGuessIdException::class);
+        $this->expectExceptionMessage('The id \'invalid\' is not a guess id');
+
+        GuessId::createFromString('invalid');
+    }
+
+    public function testCreateFromValidString(): void
+    {
+        $id = GuessId::create();
+
+        $guessId = GuessId::createFromString($id->value());
+
+        self::assertEquals($id, $guessId);
+    }
+}
